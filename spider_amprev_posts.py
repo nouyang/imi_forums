9469b5644566269368f5a049e2036a9f7fe265be
nouyang <nouyang@alum.mit.edu>
2023-02-06 13:16:31 -0500

    debugged dict (for yield) creator, working post content scraper

--- spider_amprev_posts.py
+++ spider_amprev_posts.py
@@ -19,12 +19,11 @@ class PostSpider(CrawlSpider):
         filename='log.txt',
         format='[%(asctime)s] %(levelname)s: %(message)s',
         datefmt='%d/%b/%Y %H:%M:%S',
-        level=logging.info
+        level=logging.INFO # NOTE: doesn't actually do anything
     )
     name = 'extract_posts'
 
     def start_requests(self):
-        self.cities_crawled = set()
         self.base_url = 'https://ampreviews.net'
 
         post_url = '/index.php?threads/building-a-list-of-he-only-spots-in-manhattan.129971/' 
@@ -33,87 +32,139 @@ class PostSpider(CrawlSpider):
         yield scrapy.Request(url=url, callback=self.parse_page)
 
     def parse_page(self, response):
-        max_pages = response.css('li.pageNav-page:last-child a::text').get()
-
-        category_text = response.css('.p-title-value::text').get() # e.g. Discussion-Dallas
-        self.cities_crawled.add(category_text.split(' - ')[-1]) # e.g. Dallas 
-        self.state['cities_crawled'] = self.cities_crawled
-
         page_name_and_pagination = response.css('title::text').get() # e.g. Discussion-Dallas | Page 2 | AMPReviews
+        max_pages = response.css('li.pageNav-page:last-child a::text').get()
         page_url = response.url  
+        page_name = response.css('.p-title-value::text').get()
 
-        posts = response.css('article.message--post').get()
 
+        # - get post metadata
+        posts = response.css('article.message--post')
+        self.logger.error('hi')
         for post in posts: 
-            #for post in response.css('div.bbWrapper'):
+            post_id = post.css('::attr(data-content)').get()
+            # NOTE unclosed parenthesis #ERROR:  
+            # cssselect.parser.SelectorSyntaxError: Expected an argument, got <EOF at 18>
 
-            # - get post metadata
-            post_link = post.css 
-            post_id = post.css('::attr(data-content').get()
             # example link: "/index.php?threads/building-a-list-of-he-only-spots-in-manhattan.129971/post-954437" 
             
-            posted_date_readable = post.css('div.message-date time::attr(title)').get()
-            posted_date_data = post.css('div.message-date time::attr(title)').get()
-            post_num = post.css('.message-attribution-opposite a::text').get().strip().split()[-1]
+            # -- POST STATS
+            self.logger.error('hi?')
+            posted_date_readable = post.css('.message-date time::attr(title)').get()
+            posted_date_data = post.css('.message-date time::attr(title)').get()
+            post_ordinal = post.css('.message-attribution-opposite a::text').get() # example: #19 
+            post_ordinal = post_ordinal[1:] if post_ordinal else None # in case CSS selector fails / website changes, scraper so not completely fail
 
-            post_text = post.css('div.bbWrapper::text').getall()
 
-            quotes = post.css('div.bbCodeBlock--quote').get()
-            # WHAT IF THERE'S TWO QUOTES 0:
+            # -- POST TEXT 
+            self.logger.error('hi2')
+            post_text = ''.join(post.css('div.bbWrapper::text').getall())
 
+            # -- QUOTES: handle quotes if exist
+            quotes = post.css('div.bbCodeBlock--quote')
+            num_quotes = len(quotes) # store as checksum in case want to unconcatenate quotes
+
+            self.logger.error('hi3')
+            quoted_post_ids = []
+            quoted_authors = []
+            quoted_contents = []
             if quotes: 
-                num_quotes = 0
-                quoted_post_ids = []
-                quoted_authors = []
-                quoted_content = []
                 for quote in quotes:
-                    num_quotes += 1
-                    quoted_post_id = quote.css('.bbCodeBlock-sourceJump::attr(data-content-selector)').extract()
-                    quoted_post_ids.append(quoted_post_id)
-
-                    quoted_author = quote.css('a::text').get().split()[0]
-                    quoted_authors.append(quoted_author)
+                    # example output: <a href="/index.php?goto/post&amp;ipostd=955852" class="bbCodeBlock-sourceJump" data-xf-click="attribution" data-content-selector="#post-955852">XYZ said:</a>
+                    quoted_post_id = quote.css('.bbCodeBlock-sourceJump::attr(data-content-selector)').get()
+                    quoted_author = quote.css('a::text').get()  #.replace(' said:', '')
+                    quoted_author = quoted_author.split()[0] if quoted_author else None
                     quoted_post_content = quote.css(
                         '.bbCodeBlock-expandContent::text').get()   
-                    quoted_content.append(quoted_post_content)
-                ' ~~ '.join(quoted_ids) 
-                ' ~~ '.join(quoted_authors) 
-                ' ~~ '.join(quoted_content) 
-            likers = nopost.css('div.likesBar a * ::text').getall()
+
+                    quoted_authors.append(quoted_author)
+                    quoted_post_ids.append(quoted_post_id)
+                    quoted_contents.append(quoted_post_content)
+                ' ~!~ '.join(quoted_post_ids) 
+                ' ~!~ '.join(quoted_authors) 
+                ' ~!~ '.join(quoted_contents) 
+                
+            # --  LIKES
+            self.logger.error('hi3')
+            likers = post.css('div.likesBar a * ::text').getall()
+            num_likers = len(likers) # could be interesting stat
             likers = ' - '.join(likers)
-            # If want ot parse newlines in html, and ignore other text: In [72]: ' '.join(quote.xpath('//div[@class="bbCodeBlock-expandContent"]//text()').getall())
 
-            # - Author stats 
-            author = post.css('::attr(data-author').get()
-            author_url = post.css('div.message-userDetails a.username::attr(href)').get()
-            author_title, author_num_posts, author_num_reviews = post.css('h5.message-userTitle ::text').getall()[:3]
-            # Example output: ['Review Contributor', 'Messages: 46', 'Reviews: 13', 'Joined ', 'Oct 2, 2019']
+            # -- AUTHOR STATS
+            self.logger.error('hi4')
+            author = post.css('::attr(data-author)').get()
+            author_url = post.css('a.username::attr(href)').get()
+            author_title, author_num_posts, author_num_reviews = None, None, None 
+            try:
+                author_title, author_num_posts, author_num_reviews = post.css(
+                    '.userTitle ::text').getall()[:3]
+                    # Example output: ['Review Contributor', 'Messages: 46', 'Reviews: 13',# 'Joined ', 'Oct 2, 2019']
+                author_num_posts = author_num_posts.split()[-1]
+                author_num_reviews = author_num_reviews.split()[-1]
+            except: # May produce indexing error if no results
+                self.logger.error('Attempt to extract author stats failed')
+
             join_date_readable = post.css('.userTitle time::attr(title)').get()
-            join_date_data  = post.css('div.message-userDetails .userTitle time::attr(data-time)').get()
+            join_date_data  = post.css('.userTitle time::attr(data-time)').get()
 
+            self.logger.info(f'Now scraped: {page_name_and_pagination} -- {page_url} -- TotalPages {max_pages}')
 
+            # -- YIELD
+            self.logger.error('hi yield')
+            # create dictionary to yield
+            loc = locals()
+            self.logger.debug(post_id)
+            fields = '''
+                post_id
+                post_text
+                posted_date_readable
+                posted_date_data 
+                post_ordinal
+                post_text
 
-            self.logger.info(f'Now scraping: {page_name_and_pagination} -- {page_url} -- TotalPages {max_pages}')
+                author
+                author_url
+                author_title 
+                author_num_posts 
+                author_num_reviews
+                join_date_readable
+                join_date_data
 
-            scraped = {
-                'title':title, 
-        
-                'comment': category_text,
-            }
-            yield scraped
+                quoted_post_ids
+                quoted_authors
+                quoted_contents
+                num_quotes
+                likers
+                num_likers 
+            '''.split()
 
+            fields = '''
+                post_id
+            '''.split()
+            #fields = [field.strip() for field in fields]
+            fields = set(fields) # prevent dup key error
+            self.logger.debug(fields)
+
+            page_data = {key: loc[key] for key in loc.keys() if key in fields}
+            page_data['comment'] = page_name
+
+            yield page_data
+
+'''
         # dirty hack to insert "comment" into bottom of csv file
         # which contains the current page and url, just in case
-
+        self.logger.error('hi yield final')
         yield {
             'comment': f'{page_name_and_pagination} -- {page_url} -- TotalPages {max_pages}'
         }
 
         next_page = response.css('a.pageNav-jump--next::attr(href)').get()
+        self.logger.error('hi nextPAge')
         if next_page is not None:
             next_page = response.urljoin(next_page)
             self.logger.info(f'going to next page: {next_page}')
             yield scrapy.Request(next_page, callback=self.parse_page)
+'''
 
 c = CrawlerProcess(
     settings={
@@ -125,10 +176,10 @@ c = CrawlerProcess(
         "CONCURRENT_REQUESTS":1, # default 16
         "CONCURRENT_REQUESTS_PER_DOMAIN":1, # default 8 
         "CONCURRENT_ITEMS":1, # DEFAULT 100
-        "DOWNLOAD_DELAY": 10, # default 0
+        "DOWNLOAD_DELAY": 3, # default 0
         "DEPTH_LIMIT":1,
-        "JOBDIR":'crawls/amprev_threads',
-        "DUPEFILTER_DEBUG":True,
+        #"JOBDIR":'crawls/amprev_posts',
+        #"DUPEFILTER_DEBUG":True,
     }
 )
 
