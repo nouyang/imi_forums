ebe3ac20c1aa147c6c19ef806d467dbb0f8b417b
nouyang <nouyang@alum.mit.edu>
2023-02-06 17:35:20 -0500

    added params for init scrape of post data

--- spider_amprev_posts.py
+++ spider_amprev_posts.py
@@ -25,11 +25,13 @@ class PostSpider(CrawlSpider):
 
     def start_requests(self):
         self.base_url = 'https://ampreviews.net'
+        forum_data = pd.read_csv('bkup_data/_raw_forum.csv')
 
-        post_url = '/index.php?threads/building-a-list-of-he-only-spots-in-manhattan.129971/' 
-        url = self.base_url + post_url 
-        self.logger.error(f'now working with url {url}')
-        yield scrapy.Request(url=url, callback=self.parse_page)
+        #post_url = '/index.php?threads/building-a-list-of-he-only-spots-in-manhattan.129971/' 
+        #url = self.base_url + post_url 
+        for category_url in forum_data.link:
+            url = self.base_url + category_url
+            self.logger.error(f'now working with url {url}')
 
     def parse_page(self, response):
         page_name_and_pagination = response.css('title::text').get() # e.g. Discussion-Dallas | Page 2 | AMPReviews
@@ -37,10 +39,8 @@ class PostSpider(CrawlSpider):
         page_url = response.url  
         page_name = response.css('.p-title-value::text').get()
 
-
         # - get post metadata
         posts = response.css('article.message--post')
-        self.logger.error('hi')
         for post in posts: 
             post_id = post.css('::attr(data-content)').get()
             # NOTE unclosed parenthesis #ERROR:  
@@ -49,22 +49,19 @@ class PostSpider(CrawlSpider):
             # example link: "/index.php?threads/building-a-list-of-he-only-spots-in-manhattan.129971/post-954437" 
             
             # -- POST STATS
-            self.logger.error('hi?')
             posted_date_readable = post.css('.message-date time::attr(title)').get()
-            posted_date_data = post.css('.message-date time::attr(title)').get()
+            posted_date_data = post.css('.message-date time::attr(data-time)').get()
             post_ordinal = post.css('.message-attribution-opposite a::text').get() # example: #19 
             post_ordinal = post_ordinal[1:] if post_ordinal else None # in case CSS selector fails / website changes, scraper so not completely fail
 
 
             # -- POST TEXT 
-            self.logger.error('hi2')
             post_text = ''.join(post.css('div.bbWrapper::text').getall())
 
             # -- QUOTES: handle quotes if exist
             quotes = post.css('div.bbCodeBlock--quote')
             num_quotes = len(quotes) # store as checksum in case want to unconcatenate quotes
 
-            self.logger.error('hi3')
             quoted_post_ids = []
             quoted_authors = []
             quoted_contents = []
@@ -85,13 +82,11 @@ class PostSpider(CrawlSpider):
                 ' ~!~ '.join(quoted_contents) 
                 
             # --  LIKES
-            self.logger.error('hi3')
             likers = post.css('div.likesBar a * ::text').getall()
             num_likers = len(likers) # could be interesting stat
             likers = ' - '.join(likers)
 
             # -- AUTHOR STATS
-            self.logger.error('hi4')
             author = post.css('::attr(data-author)').get()
             author_url = post.css('a.username::attr(href)').get()
             author_title, author_num_posts, author_num_reviews = None, None, None 
@@ -102,7 +97,7 @@ class PostSpider(CrawlSpider):
                 author_num_posts = author_num_posts.split()[-1]
                 author_num_reviews = author_num_reviews.split()[-1]
             except: # May produce indexing error if no results
-                self.logger.error('Attempt to extract author stats failed')
+                self.warning.error('Attempt to extract author stats failed')
 
             join_date_readable = post.css('.userTitle time::attr(title)').get()
             join_date_data  = post.css('.userTitle time::attr(data-time)').get()
@@ -110,66 +105,68 @@ class PostSpider(CrawlSpider):
             self.logger.info(f'Now scraped: {page_name_and_pagination} -- {page_url} -- TotalPages {max_pages}')
 
             # -- YIELD
-            self.logger.error('hi yield')
             # create dictionary to yield
             loc = locals()
-            self.logger.debug(post_id)
+
             fields = '''
-                post_id
-                post_text
-                posted_date_readable
-                posted_date_data 
                 post_ordinal
-                post_text
+                post_id
+                posted_date_readable
 
                 author
                 author_url
                 author_title 
                 author_num_posts 
                 author_num_reviews
+
+                num_quotes
+                quoted_post_ids
+                quoted_authors
+                likers
+                num_likers 
+
                 join_date_readable
                 join_date_data
 
-                quoted_post_ids
-                quoted_authors
-                quoted_contents
-                num_quotes
-                likers
-                num_likers 
-            '''.split()
+                posted_date_data 
 
-            fields = '''
-                post_id
             '''.split()
-            #fields = [field.strip() for field in fields]
-            fields = set(fields) # prevent dup key error
+            _ = '''
+                post_text
+                quoted_contents
+            '''
+
+            #fields = '''
+                #post_id
+            #'''.split()
+            #fields = set(fields) # prevent dup key error
             self.logger.debug(fields)
 
-            page_data = {key: loc[key] for key in loc.keys() if key in fields}
-            page_data['comment'] = page_name
+            # page_data = {key: loc[key] for key in loc.keys() if key in fields}
+            # retain ordering
+            page_data = {}
+            for key in fields:
+                page_data[key] = loc[key] 
 
+            page_data['comment'] = page_name
             yield page_data
 
-'''
         # dirty hack to insert "comment" into bottom of csv file
         # which contains the current page and url, just in case
-        self.logger.error('hi yield final')
         yield {
             'comment': f'{page_name_and_pagination} -- {page_url} -- TotalPages {max_pages}'
         }
 
         next_page = response.css('a.pageNav-jump--next::attr(href)').get()
-        self.logger.error('hi nextPAge')
         if next_page is not None:
             next_page = response.urljoin(next_page)
             self.logger.info(f'going to next page: {next_page}')
             yield scrapy.Request(next_page, callback=self.parse_page)
-'''
 
 c = CrawlerProcess(
     settings={
         "FEEDS":{
-            "_tmp_threads.csv" : {"format" : "csv",
+            "_tmp_posts.csv" : {"format" : "csv",
                                 "overwrite":True,
                                 "encoding": "utf8",
                             }},
