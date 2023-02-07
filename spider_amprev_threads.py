72e33025b7c1c416c8b46e31f96fb54e5549f453
nouyang <nouyang@alum.mit.edu>
2023-02-07 10:35:41 -0500

    fixed rebase overwrite hopefully?

--- spider_amprev_threads.py
+++ spider_amprev_threads.py
@@ -19,7 +19,7 @@ class ThreadSpider(CrawlSpider):
         filename='log.txt',
         format='[%(asctime)s] %(levelname)s: %(message)s',
         datefmt='%d/%b/%Y %H:%M:%S',
-        level=logging.info
+        level=logging.INFO
     )
     name = 'extract_threads'
 
